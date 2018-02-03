0b9e8c39e9a2b77f9550cfb89492c0d5a9965c4d
Yurets <dr.yurets@gmail.com>
2018-02-03 14:30:47 +0300

    030218 фавиконы, подправлена шапка, поддомен для CSS

--- wp-content/themes/armada/header.php
+++ wp-content/themes/armada/header.php
@@ -1,17 +1,27 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?> >
+<!doctype html>
+<html lang="ru" dir="ltr">
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <a href="https://msdn.microsoft.com/en-us/library/jj676915%28v=vs.85%29.aspx"></a>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<?php
-
-		$appointment_options=theme_setup_data();
-		$header_setting = wp_parse_args(  get_option( 'appointment_options', array() ), $appointment_options);
-		if($header_setting['upload_image_favicon']!=''){ ?>
-            <link rel="shortcut icon" href="<?php  echo $header_setting['upload_image_favicon']; ?>" />
-		<?php } ?>
-    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
     <link href="https://use.fontawesome.com/releases/v5.0.1/css/all.css" rel="stylesheet">
     <?php wp_head(); ?>
 </head>
